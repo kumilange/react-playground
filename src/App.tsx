cb263cfa9bfd555849a9436ae1fe26ffc4ef3f78
Kumiko Haraguchi <haraguchi935@gmail.com>
2024-03-27 16:21:34 -0600

    Add markups

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,44 @@
-import styles from './App.module.css';
+import styles from './App.module.scss';
+import iconClip from './assets/icon_clip.svg';
+import iconMailSp from './assets/icon_mail_sp.svg';
 
 function App() {
 	return (
 		<main>
-			<h1 className={styles.heading1}>React App</h1>
+			<h1 className={styles.heading1}>Mail Archiver</h1>
+			{/* input */}
+			<div>calender</div>
+
+			{/* result list */}
+			<div>
+				<h2 className={styles.heading2}>Results: <span className={styles.emp}>10</span>mail(s)</h2>
+				{/* header */}
+				<ul className={styles.flexRow}>
+					<li>From</li>
+					<li>To</li>
+					<li>Subject</li>
+					<li>Date</li>
+				</ul>
+				{/* result list */}
+				<ul>
+					<li>
+						<div className={styles.flexRow}>
+							<div className={`${styles.mailContent} ${styles.position}`}>
+								<div className={styles.flexRow}>
+									<img src={iconMailSp} alt="" className={styles.iconMailSp} />
+									<div className={styles.mailContent}>
+										<p>aaa@example.com</p>
+										<p>zzz.zzz@example </p>
+									</div>
+								</div>
+								<p style={{ paddingRight: "1.5rem" }}>[HR-887] Notice of official announcement</p>
+								<img src={iconClip} alt="" className={styles.iconClip} />
+							</div>
+							<p>0:20</p>
+						</div>
+					</li>
+				</ul>
+			</div>
 		</main>
 	);
 }
