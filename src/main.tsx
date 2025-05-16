c8f771fa03bd507ecd0f9aae60b95be14790b1b0
Kumiko Haraguchi <haraguchi935@gmail.com>
2025-05-16 14:10:51 -0600

    Modulize FileExplorer

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Workspace } from './Workspace/Workspace';
+import { Workspace } from './componentsTemp/FileExplorer/Workspace/Workspace';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
